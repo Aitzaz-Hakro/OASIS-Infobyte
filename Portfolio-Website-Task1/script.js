6a13e4bbdc952ca7bbdb47d3c3aef9c97571dbc6
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-01-23 22:05:08 +0500

    last update

--- Portfolio-Website-Task1/script.js
+++
@@ -1,3 +0,0 @@
-<a ><img src="media/linkedin.svg" alt="linkedin" class=" logo1" ></a>
-<a href="https://github.com/Aitzaz-Hakro"><img src="media/github.svg" alt="linkedin" class=" logo1" ></a>
-<a href=""><img src="media/instagram.svg" alt="linkedin" class=" logo1" ></a>
